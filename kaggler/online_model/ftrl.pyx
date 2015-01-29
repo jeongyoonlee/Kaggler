86408d8dface0c92afe558193099269aeabda988
Jeong-Yoon Lee <jeong@conversionlogic.com>
2015-01-28 21:46:41 -0800

    speed up ftrl's cython code

--- kaggler/online_model/ftrl.pyx
+++ kaggler/online_model/ftrl.pyx
@@ -1,18 +1,46 @@
 from __future__ import division
-from math import sqrt
+import numpy as np
 
-from ..util import sigm
+cimport cython
+from libc.math cimport exp, sqrt
+cimport numpy as np
 
 
-class FTRL(object):
+np.import_array()
+
+cdef inline double double_max(double a, double b): return a if a >= b else b
+cdef inline double double_min(double a, double b): return a if a <= b else b
+
+
+cdef double sigm(double x):
+    return 1 / (1 + exp(-double_max(double_min(x, 20.0), -20.0)))
+
+
+cdef class FTRL:
     """FTRL online learner with the hasing trick using liblinear format data.
     
     inspired by Kaggle user tinrtgu's code at http://goo.gl/K8hQBx
     original FTRL paper is available at http://goo.gl/iqIaH0
     """
 
-    def __init__(self, alpha=0.01, beta=1., l1=1., l2=1., N=2**20,
-                 interaction=True):
+    cdef double alpha      # learning rate
+    cdef double beta
+    cdef double l1
+    cdef double l2
+    cdef unsigned int N              # # of features
+    cdef bint interaction
+    cdef double[:] w
+    cdef double[:] c
+    cdef double[:] z
+    #cdef dict z
+
+    def __init__(self,
+                 double alpha=0.01,
+                 double beta=1.,
+                 double l1=1.,
+                 double l2=1.,
+                 unsigned int N=2**20,
+                 bint interaction=True):
         self.alpha = alpha      # learning rate
         self.beta = beta
         self.l1 = l1
@@ -21,11 +49,17 @@ class FTRL(object):
         self.interaction = interaction
 
         # initialize weights and counts
-        self.w = [0.] * self.N
-        self.c = [0.] * self.N
-        self.z = {}
+        self.w = np.zeros((self.N,), dtype=np.float64)
+        self.c = np.zeros((self.N,), dtype=np.float64)
+        self.z = np.zeros((self.N,), dtype=np.float64)
+        #self.z = {}
 
-    def _indices(self, x):
+    def _indices(self, list x):
+        cdef unsigned int index
+        cdef int l
+        cdef int i
+        cdef int j
+
         yield 0
 
         for index in x:
@@ -38,7 +72,7 @@ class FTRL(object):
                 for j in xrange(i + 1, l):
                     yield abs(hash('{}_{}'.format(x[i], x[j]))) % self.N
 
-    def get_x(self, xs):
+    def get_x(self, list xs):
         """Apply hashing trick to a dictionary of {feature name: value}.
 
         Args:
@@ -55,26 +89,31 @@ class FTRL(object):
 
         return x
 
-    def update(self, x, yhat, y):
+    def update(self, list x, double p, double y):
         """Update the model.
 
         Args:
             idx - a list of index of non-zero features
             val - a list of values of non-zero features
-            yhat - prediction of the model
+            p - prediction of the model
             y - true target value
 
         Returns:
             updates model weights and counts
         """
-        g = yhat - y
+        cdef int i
+        cdef double g
+        cdef double g2
+        cdef double s
+
+        g = p - y
         g2 = g * g
         for i in self._indices(x):
             s = (sqrt(self.c[i] + g2) - sqrt(self.c[i])) / self.alpha
             self.w[i] += g - s * self.z[i]
             self.c[i] += g2
 
-    def predict(self, x):
+    def predict(self, list x):
         """Predict for features.
 
         Args:
@@ -84,18 +123,23 @@ class FTRL(object):
         Returns:
             a prediction for input features
         """
-        z = {}
+        cdef int i
+        cdef double sign
+        cdef double wTx
+        #cdef dict z
+
+        #z = {}
         wTx = 0.
         for i in self._indices(x):
             sign = -1. if self.w[i] < 0 else 1.
             if sign * self.w[i] <= self.l1:
-                z[i] = 0.
+                self.z[i] = 0.
             else:
-                z[i] = (sign * self.l1 - self.w[i]) / \
-                        ((self.beta + sqrt(self.c[i])) / self.alpha + self.l2)
+                self.z[i] = (sign * self.l1 - self.w[i]) / \
+                            ((self.beta + sqrt(self.c[i])) / self.alpha + self.l2)
 
-            wTx += z[i]
+            wTx += self.z[i]
 
-        self.z = z
+        #self.z = z
 
         return sigm(wTx)
--- setup.py
+++ setup.py
@@ -1,21 +1,38 @@
-from distutils.core import setup
+from setuptools import setup, Extension
+from Cython.Distutils import build_ext
+
+import numpy as np
+
 
 setup(
     name='Kaggler',
     version='0.2.dev',
+
     author='Jeong-Yoon Lee',
     author_email='jeongyoon.lee1@gmail.com',
+
     packages=['kaggler',
               'kaggler.model',
               'kaggler.online_model',
               'kaggler.test'],
     url='http://pypi.python.org/pypi/Kaggler/',
     license='LICENSE.txt',
+
     description='Code for Kaggle Data Science Competitions.',
     long_description=open('README.txt').read(),
+
     install_requires=[
-        "scipy >= 0.14.0",
-        "scikit-learn >= 0.15.0",
-        "statsmodels >= 0.5.0",
+        'cython',
+        'numpy',
+        'scipy >= 0.14.0',
+        'scikit-learn >= 0.15.0',
+        'statsmodels >= 0.5.0',
     ],
+
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[Extension('kaggler.online_model.ftrl',
+                           ['kaggler/online_model/ftrl.pyx'],
+                           libraries=[],
+                           include_dirs=[np.get_include()],
+                           extra_compile_args=['-O3'])],
 )
