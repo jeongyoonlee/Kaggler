4fe121c66ed0b354ffe25a450e4620d67e875616
Jeong-Yoon Lee <jeong@uber.com>
2019-05-17 16:10:04 -0700

    add import kaggler to docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,7 @@ matplotlib.use('agg')
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath("../.."))
 
+import kaggler
 import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
